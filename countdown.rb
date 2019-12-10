3460bd0999c4e5161a4459a14155d8f63248d0da
Felicia Smith <smithfn1012@gmail.com>
2019-12-10 19:58:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,11 @@
 def countdown(number)
   number = 10
   while number < 10
-  puts "#{number} SECOND(S)!"
-  counter -= 1
-  until number == 0 
-  puts "HAPPY NEW YEAR!"
+    puts "#{number} SECOND(S)!"
+      counter -= 1
+    until number == 0 
+      puts "HAPPY NEW YEAR!"
+    end
   end
 end
 
