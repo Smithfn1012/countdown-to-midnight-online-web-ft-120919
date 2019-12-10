12ac65de26cfae4a5f3b54fd719a3e234a4a3589
Felicia Smith <smithfn1012@gmail.com>
2019-12-10 20:06:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,9 +6,8 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
       number -= 1
   end
-end
-    
     return "HAPPY NEW YEAR!"
+  end
     
     def countdown_with_sleep(number)
       number = 10
